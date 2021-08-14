2feff56b68ea9e41659916f26c57f30cca064ef4
Lindenbar <bor.tim.1997@gmail.com>
2021-08-14 23:49:08 +0300

    Added portfolio project (header)

---
+++ js/main-functions.js
@@ -0,0 +1,24 @@
+//< Загрузка html документов в теги import, где атрибут "file" - путь к html файлу
+$(function importHTML(findPlace = $('html')) {
+    let importPlaces = findPlace.find('import');
+
+    if (importPlaces[0]) {
+        $(importPlaces).each((i, importTag) => {
+            let file = importTag.getAttribute('file');
+            findPlace = $(importTag.parentNode);
+            console.log(findPlace);
+            importTag = $(importTag).load(file, () => {
+                importTag.replaceWith(importTag.html());
+                /* В импортированных файлах, тоже может быть импорт, поэтому снова вызываем функцию.
+                 * Если импорт-тэг найден, то подгружаем файл, иначе ничего не происходит.
+                 * Функция будет самовоспроизводится пока не пройдется по всем импорт-тегам, которые
+                 * находятся в файле вызова скрипта и в файлах которые были импортированы.
+                 */
+                importHTML(findPlace);
+            });
+        });
+    } else {
+        console.log('imported');
+    }
+});
+//>
